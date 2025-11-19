4a65a4c8ba0ab398b5cbeecc23ea4ca99aa16bc5
uiop <uiop@wasdhjkl.xyz>
2025-11-19 15:58:01 -0500

    feat(bootloader): setup 2MB page translation

--- kern_entry.asm
+++ kern_entry.asm
@@ -9,13 +9,14 @@
 PML4T_ADDR equ 0x1000
 PDPT_ADDR  equ 0x2000
 PDT_ADDR   equ 0x3000
-PT_ADDR    equ 0x4000
 
 PTT_SIZE  equ 4096
 PTT_ENTS  equ 512
 PTTE_SIZE equ 8
+
 PTT_P     equ 1
 PTT_RW    equ 2
+PTT_PS    equ (1 << 7)
 
 [bits 32]
 section .text
@@ -40,24 +41,41 @@ _start:
     or    eax, 0x20 ; CR4.PAE
     mov   cr4, eax
 
+    ;;
+    ;; Clear page translation tables
+    ;;
     mov   edi, PML4T_ADDR
     mov   cr3, edi
     xor   eax, eax
-    mov   ecx, PTT_SIZE
+    mov   ecx, 0x6000 / 4 ; 24KB / 4 bytes (PML4 + PDPT + 4 PDTs)
     rep   stosd
-    mov   edi, cr3
+
+    ;;
+    ;; PML4[0] -> PDPT
+    ;;
+    mov   edi, PML4T_ADDR
     mov   dword [edi], PDPT_ADDR | PTT_P | PTT_RW
+
+    ;;
+    ;; PDPT[0-3] -> 4 PDTs (each covers 1GB)
+    ;;
     mov   edi, PDPT_ADDR
     mov   dword [edi], PDT_ADDR | PTT_P | PTT_RW
+    mov   dword [edi + 8], (PDT_ADDR + PTT_SIZE) | PTT_P | PTT_RW
+    mov   dword [edi + 16], (PDT_ADDR + PTT_SIZE * 2) | PTT_P | PTT_RW
+    mov   dword [edi + 24], (PDT_ADDR + PTT_SIZE * 3) | PTT_P | PTT_RW
+
+    ;;
+    ;; Fill all 4 PDTs with 2MB pages
+    ;;
     mov   edi, PDT_ADDR
-    mov   dword [edi], PT_ADDR | PTT_P | PTT_RW
-    mov   edi, PT_ADDR
-    mov   ebx, PTT_P | PTT_RW
-    mov   ecx, PTT_ENTS
+    xor   ebx, ebx
+    or    ebx, PTT_P | PTT_RW | PTT_PS
+    mov   ecx, PTT_ENTS * 4
   .loop:
-    mov   dword [edi], ebx
-    add   ebx, PTT_SIZE
-    add   edi, PTTE_SIZE
+    mov   [edi], ebx
+    add   ebx, 0x200000 ; Next 2MB phys frame
+    add   edi, 8
     loop  .loop
 
     mov   ecx, 0xC0000080
